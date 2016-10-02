8f8d685364d6cc687c88361d2dd8e10087f90a68
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2016-10-02 22:44:05 +0200

    added bokeh reference

---
+++ code/py/bokeh_sandbox.py
@@ -0,0 +1,6 @@
+# http://bokeh.pydata.org/en/latest/docs/user_guide/setup.html#userguide-setup
+from bokeh.plotting import figure, output_file, show
+output_file("test.html")
+p = figure()
+p.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=2)
+show(p)
